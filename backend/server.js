698d158ecce772273fe19be7eb518f659897abfe
Manik kori <manikkori697@gmail.com>
2026-07-21 20:00:36 +0530

    create authRoutes.js and update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use('/api/auth', require('./routes/authRoutes'));
+
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
